ae06bdee0a844f996c56645f91ef02c5ffee9dac
Gustavo Caravajal <Gustavocarvajal_2001@outlook.es>
2026-01-19 20:07:20 -0300

    solucionar problemas de estilos en el menu hamburguesa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
 import "./Header.css"
+import { useState } from "react";
 export const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+        scrollToTop();
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
     return (
-        <header>
+        <header className={showMenu ? "menu-open" : ""}>
             <Link to={"/"}>
                 <img className="logo" src="/logo.png" alt="logo de la aplicacion de peliculas" />
             </Link>
-            <Nav />
+            <Nav showMenu={showMenu} toggleMenu={toggleMenu} scrollToTop={scrollToTop}/>
         </header>
     );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,24 @@
 import { Link } from "react-router-dom";
 import "./Nav.css"
-import { useState } from "react";
 
-export const Nav = () => {
-    const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu);
-    }
-
+export const Nav = ({showMenu, toggleMenu, scrollToTop}) => {
+ 
     return <>
         <nav className="nav-desktop">
             <ul className="nav-items">
                 <li className="item">
-                    <Link className="link" to={"/"}>
+                    <Link onClick={scrollToTop} className="link" to={"/"}>
                         Home
                     </Link>
                 </li>
                 <li className="item">
-                    <Link className="link" to={"/movie"}>
+                    <Link onClick={scrollToTop} className="link" to={"/movie"}>
                         Movies
                     </Link>
                 </li>
                 <li className="item">
-                    <Link className="link" to={"/tv"}>
+                    <Link onClick={scrollToTop} className="link" to={"/tv"}>
                         Tv Series
                     </Link>
                 </li>
@@ -37,26 +32,26 @@ export const Nav = () => {
                 </div>
                 :
                 <nav className="nav-mobile">
-                    <Link to={"/"}>
-                        <img className="logo-mobile" src="/logo.png" alt="logo de la aplicacion de peliculas" />
+                    <Link onClick={scrollToTop} to={"/"}>
+                        <img className="logo-mobile" src="/public/logo.png" alt="logo de la aplicacion de peliculas" />
                     </Link>
                     <button className="hamburger-button" onClick={() => toggleMenu()}>☰</button>
                     <h4 className="menu-title">Menu</h4>
                     <ul className="nav-items">
                         <li className="item" onClick={() => toggleMenu()}>
-                            <img className="item-logo" src="/home.png" alt="home" />
+                            <img className="item-logo" src="/public/home.png" alt="home" />
                             <Link className="link" to={"/"}>
                                 Home
                             </Link>
                         </li>
                         <li className="item" onClick={() => toggleMenu()} >
-                            <img className="item-logo" src="/movies.png" alt="movie" />
+                            <img className="item-logo" src="/public/movies.png" alt="movie" />
                             <Link className="link" to={"/movie"}>
                                 Movies
                             </Link>
                         </li>
                         <li className="item" onClick={() => toggleMenu()} >
-                            <img className="item-logo" src="/series.png" alt="serie" />
+                            <img className="item-logo" src="/public/series.png" alt="serie" />
                             <Link className="link" to={"/tv"}>
                                 Tv Series
                             </Link>
